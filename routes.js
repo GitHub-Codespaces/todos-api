ada7fd026c772710a5797f2302503dd5e2b56055
Fikayo Adepoju <fikfikky@gmail.com>
2022-08-25 12:02:37 +0000

    Changed welcome message

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.send("Welcome to the Todos API");
+  res.send("Remote Interface for To-Do Items");
 });
 
 router.post("/add-item", async function(req, res) {
